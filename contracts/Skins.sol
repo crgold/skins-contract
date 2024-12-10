637e4c9f1f4d4e2c6caec925605a12f48da8458b
CHRISTOPHER GOLD <cgoldaz@gmail.com>
2024-12-11 04:07:53 +0800

    init

--- contracts/Skins.sol
+++ contracts/Skins.sol
@@ -2,8 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC721Drop.sol";
+import "@thirdweb-dev/contracts/eip/interface/IERC721Enumerable.sol";
 
-contract Skins is ERC721Drop {
+contract Skins is ERC721Drop, IERC721Enumerable {
+    uint256[] private _allTokens;
+    mapping(address => uint256[]) private _ownedTokens;
+    mapping(uint256 => uint256) private _ownedTokensIndex;
+    mapping(uint256 => uint256) private _allTokensIndex;
+
     constructor(
         address _defaultAdmin,
         string memory _name,
@@ -21,4 +27,72 @@ contract Skins is ERC721Drop {
             _primarySaleRecipient
         )
     {}
+
+    function tokenByIndex(uint256 index) external view override returns (uint256) {
+        require(index < _allTokens.length, "Index out of bounds");
+        return _allTokens[index];
+    }
+
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view override returns (uint256) {
+        require(index < _ownedTokens[owner].length, "Index out of bounds");
+        return _ownedTokens[owner][index];
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+
+        if (from == address(0)) {
+            _addTokenToAllTokensEnumeration(tokenId);
+        } else if (from != to) {
+            _removeTokenFromOwnerEnumeration(from, tokenId);
+        }
+        if (to == address(0)) {
+            _removeTokenFromAllTokensEnumeration(tokenId);
+        } else if (to != from) {
+            _addTokenToOwnerEnumeration(to, tokenId);
+        }
+    }
+
+    function _addTokenToOwnerEnumeration(address to, uint256 tokenId) private {
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
+        _ownedTokens[to].push(tokenId);
+    }
+
+    function _addTokenToAllTokensEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
+        _allTokens.push(tokenId);
+    }
+
+    function _removeTokenFromOwnerEnumeration(address from, uint256 tokenId) private {
+        uint256 lastTokenIndex = _ownedTokens[from].length - 1;
+        uint256 tokenIndex = _ownedTokensIndex[tokenId];
+
+        if (tokenIndex != lastTokenIndex) {
+            uint256 lastTokenId = _ownedTokens[from][lastTokenIndex];
+            _ownedTokens[from][tokenIndex] = lastTokenId;
+            _ownedTokensIndex[lastTokenId] = tokenIndex;
+        }
+
+        _ownedTokens[from].pop();
+        delete _ownedTokensIndex[tokenId];
+    }
+
+    function _removeTokenFromAllTokensEnumeration(uint256 tokenId) private {
+        uint256 lastTokenIndex = _allTokens.length - 1;
+        uint256 tokenIndex = _allTokensIndex[tokenId];
+
+        if (tokenIndex != lastTokenIndex) {
+            uint256 lastTokenId = _allTokens[lastTokenIndex];
+            _allTokens[tokenIndex] = lastTokenId;
+            _allTokensIndex[lastTokenId] = tokenIndex;
+        }
+
+        _allTokens.pop();
+        delete _allTokensIndex[tokenId];
+    }
 }
